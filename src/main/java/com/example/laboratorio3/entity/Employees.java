ba344055c0d6ba664f48896202246d20614cc2ad
Josue Romero <jcromero@pucp.edu.pe>
2020-05-04 19:16:50 -0500

    josue1

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -5,8 +5,27 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.util.Date;
 
-//COMPLETAR
+@Entity
+@Table(name = "employees")
 public class Employees {
 
-           //COMPLETAR
+    @Id
+    private String employeeId;
+    @Column(nullable=false)
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    @Column(nullable=false)
+    private Date hireDate;
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Jobs jobs;
+    private Float salary;
+    private Float commissionPct;
+    @ManyToOne
+    @JoinColumn(name="manager_id")
+    private Employees manager;
+
+
 }
---
+++ src/main/java/com/example/laboratorio3/entity/Jobs.java
@@ -0,0 +1,18 @@
+package com.example.laboratorio3.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "jobs")
+public class Jobs {
+
+    @Id
+    private String job_id;
+    @Column(nullable=false)
+    private  String job_title;
+    private int min_salary;
+    private int max_salary;
+}
