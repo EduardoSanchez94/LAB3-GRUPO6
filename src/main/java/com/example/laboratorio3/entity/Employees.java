df12ef7184d842e2743ef875b93911d676c550b2
lewis-coder <57689615+lewis-coder@users.noreply.github.com>
2020-05-04 19:18:34 -0500

    getters and setters

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -27,5 +27,83 @@ public class Employees {
     @JoinColumn(name="manager_id")
     private Employees manager;
 
+    public String getEmployeeId() {
+        return employeeId;
+    }
 
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Jobs getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(Jobs jobs) {
+        this.jobs = jobs;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+
+    public Float getCommissionPct() {
+        return commissionPct;
+    }
+
+    public void setCommissionPct(Float commissionPct) {
+        this.commissionPct = commissionPct;
+    }
+
+    public Employees getManager() {
+        return manager;
+    }
+
+    public void setManager(Employees manager) {
+        this.manager = manager;
+    }
 }
